8663a3d2b6354208384c79883b46d904477853ab
Geovanny Martinez <gmartinez263@toromail.csudh.edu>
2024-08-12 00:20:42 -0700

    The school search page works w/ transfer class data page

--- WebScraper.py
+++ WebScraper.py
@@ -68,11 +68,51 @@ class WebScraper:
         with open('C:\wamp64\www\ChatbotAPI\jobs.json', 'w') as file:
              json.dump(jobs, file, indent=4)
 
-    def searchAssist(self, dep) -> None:
+    def searchAssist(self) -> None:
         assistDriver = webdriver.Chrome(service=self.service, options=self.options)
-        url = "https://assist.org/transfer/results?year=74&institution=50&agreement=104&agreementType=from&view=agreement&viewBy=major&viewSendingAgreements=false"
+        url = "https://assist.org"
         assistDriver.get(url)
+
+        #wait for the search bar to load before entering 'CSUDH' into the search bar
+        WebDriverWait(assistDriver, 10).until(
+            EC.presence_of_element_located((By.ID, "governing-institution-select"))
+        ).send_keys('CSUDH')
+
+        #wait for the CSUDH option to load from the dropdown menu before clicking on it
+        WebDriverWait(assistDriver, 10).until(
+            EC.presence_of_element_located((By.ID, 'option-202'))
+        ).click()
+
+        #wait for the search bar to load before entering a CC institution
+        ccSearchBar = WebDriverWait(assistDriver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'institution-agreement'))
+        )
+
+        #type the school we want tranfer class data from and then click on the 1st option from the drop down menu
+        ccSearchBar.send_keys('Cerritos College')
+        schoolList = WebDriverWait(assistDriver, 10).until(
+            EC.presence_of_element_located((By.ID, 'cdk-overlay-1'))
+        )
+        school = schoolList.find_element(By.TAG_NAME, 'amc-option')
+        school.click()
+
+        #wait for the button to view tranfer data to be visible and clickable
+        viewTranferCourseBtn = WebDriverWait(assistDriver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'btn-primary'))
+        )
+        assistDriver.execute_script("arguments[0].scrollIntoView(true);", viewTranferCourseBtn)
+        time.sleep(2)
+        WebDriverWait(assistDriver, 10).until(
+            EC.element_to_be_clickable(viewTranferCourseBtn)
+        )
+        viewTranferCourseBtn.click()
         
+        self.getTransferData(assistDriver, 'computer science')
+
+    def getTransferData(self, assistDriver, dep) -> None:
+        #giving the website 5 seconds to load before attempting to read from it
+        time.sleep(5)
+
         #Wait for the search bar to load in before attempting to type in it
         search = WebDriverWait(assistDriver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, "ng-valid"))
@@ -86,10 +126,10 @@ class WebScraper:
         department.click()
 
         #wait for the sections tag to load before trying to read from it
-        section = WebDriverWait(assistDriver, 10).until(
+        section = WebDriverWait(assistDriver, 15).until(
             EC.presence_of_element_located((By.CLASS_NAME, "template"))
         )
-
+        
         #Get all the cc classes and their corresponding DH course
         divs = section.find_elements(By.XPATH, './div')
         for div in divs:
@@ -111,7 +151,7 @@ class WebScraper:
                     ccCourseName = ccCourse.find_element(By.CLASS_NAME, 'courseTitle').text
                     ccCourseUnits = ccCourse.find_element(By.CLASS_NAME, 'courseUnits').text
 
-                    print(f"{DHCourseNum} {DHCourseName} {DHCourseUnits} units <- {ccCourseNum} {ccCourseName} {ccCourseUnits} units\n")
+                    print(f"(CSUDH){DHCourseNum} {DHCourseName} {DHCourseUnits} units <- (cerritos college){ccCourseNum} {ccCourseName} {ccCourseUnits} units\n")
             except NoSuchElementException:
                 continue
 
